f7ed55c1f49d7e70ec96ab099eab201441857fb2
Егор <@DESKTOP-VF4U8Q2>
2024-10-27 22:21:08 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication19/ConsoleApplication19.cpp
@@ -0,0 +1,24 @@
+﻿#include <cmath>
+#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    float m, F, t;
+
+    cout << "Масса звездолёта: ";
+    cin >> m;
+    
+    cout << "Сила тяги: ";
+    cin >> F;
+   
+    cout << "Время: ";
+    cin >> t;
+
+    float a = F / m;
+    float result = 0.5 * a * pow(t, 2);
+
+    cout << "Корабль через " << t << " секунд будет на расстоянии " << result << " метров." << endl;
+
+  
+}
